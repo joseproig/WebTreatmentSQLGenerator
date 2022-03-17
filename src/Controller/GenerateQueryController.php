459fdbebb31600a1024d9713d29c99b90bd9db08
joseproig <josep.roig.torres@salle.url.edu>
2022-03-17 13:47:19 +0100

    feat: Added the logic of controller checks

--- src/Controller/GenerateQueryController.php
+++ src/Controller/GenerateQueryController.php
@@ -12,30 +12,48 @@ use DateTime;
 
 class GenerateQueryController extends AbstractController
 {
+    const EXTENSION_OF_SQLITE = 'db';
+
     #[Route('/generatequery', name: 'app_generate_query')]
     public function generateQuery(Request $request): Response
     {
         $query = new Question();
+        //Generem el formulari
         $form = $this->createForm(QuestionFormType::class,$query);
+        
+        $errorInFile = false;
 
+        //Obtenim la petició
         $form->handleRequest($request);
 
+    
+        //Comprovem si el formulari s'ha entregat i es valid, en cas contrariu es que haura entrat a la pàgina només.
         if ($form->isSubmitted() && $form->isValid()) {
-            $movie = $form->getData();
-            $image = $form->get('pathToDbFile')->getData();
-            $d1 = new Datetime();
-            $fileName =  $d1->format('U') . '.' . $image->guessExtension();
-            $image -> move(
-                $this->getParameter('kernel.project_dir') . '/public/dbs',
-                $fileName
-            );
-            $movie->setPathToDBFile($this->getParameter('kernel.project_dir') . '/public/dbs/' . $fileName);
+            $question = $form->getData();
+            $fileDB = $form->get('pathToDbFile')->getData();
             
-            return $this->redirectToRoute('app_generate_query');
+            $extension = $fileDB->getClientOriginalExtension();
+            //Movem fitxer al servidor, sols si té la extensió .db que es la que llegeix el servidor
+            if (strcmp($extension,self::EXTENSION_OF_SQLITE) == 0) {
+                //Agafem la datetime per a ficar-li el nom al fitxer que es moura al servidor
+                $d1 = new Datetime();
+                $fileName =  $d1->format('U') . '.' . $extension;
+                $pathDirectory = $this->getParameter('kernel.project_dir') . '/public/dbs'; 
+                $fileDB -> move(
+                    $pathDirectory,
+                    $fileName
+                );
+                $question->setPathToDBFile($pathDirectory . $fileName);
+
+            } else {
+                $errorInFile = true;
+            }
+            
         }
 
         return $this->renderForm("generatequerymenu.html.twig",[
-            'form' => $form
+            'form' => $form,
+            'errorInFile'=>$errorInFile
         ]);
     }
 
