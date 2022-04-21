944b148d1eb4c861b4b6cbd804d62bd0d952941f
joseproig <josep.roig.torres@salle.url.edu>
2022-04-21 15:08:49 +0200

    feat: Added Answer Entity

---
+++ src/Entity/Answer.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnswerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnswerRepository::class)]
+class Answer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'text')]
+    private $statement;
+
+    #[ORM\Column(type: 'text')]
+    private $query;
+
+    #[ORM\Column(type: 'text')]
+    private $answer;
+
+    #[ORM\ManyToOne(targetEntity: Question::class, inversedBy: 'answers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $templateQuestion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatement(): ?string
+    {
+        return $this->statement;
+    }
+
+    public function setStatement(string $statement): self
+    {
+        $this->statement = $statement;
+
+        return $this;
+    }
+
+    public function getQuery(): ?string
+    {
+        return $this->query;
+    }
+
+    public function setQuery(string $query): self
+    {
+        $this->query = $query;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(string $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public function getTemplateQuestion(): ?Question
+    {
+        return $this->templateQuestion;
+    }
+
+    public function setTemplateQuestion(?Question $templateQuestion): self
+    {
+        $this->templateQuestion = $templateQuestion;
+
+        return $this;
+    }
+}
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\QuestionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: QuestionRepository::class)]
@@ -23,6 +25,14 @@ class Question
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'questions')]
     #[ORM\JoinColumn(nullable: false)]
     private $creator;
+
+    #[ORM\OneToMany(mappedBy: 'templateQuestion', targetEntity: Answer::class)]
+    private $answers;
+
+    public function __construct()
+    {
+        $this->answers = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -61,6 +71,36 @@ class Question
     public function setCreator(?User $creator): self
     {
         $this->creator = $creator;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Answer>
+     */
+    public function getAnswers(): Collection
+    {
+        return $this->answers;
+    }
+
+    public function addAnswer(Answer $answer): self
+    {
+        if (!$this->answers->contains($answer)) {
+            $this->answers[] = $answer;
+            $answer->setTemplateQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnswer(Answer $answer): self
+    {
+        if ($this->answers->removeElement($answer)) {
+            // set the owning side to null (unless already changed)
+            if ($answer->getTemplateQuestion() === $this) {
+                $answer->setTemplateQuestion(null);
+            }
+        }
 
         return $this;
     }
